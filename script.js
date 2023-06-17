8438bc3c78a622694275768081bae0176a83f2f9
Tomáš Stýblo <119765404+Tomiczech77@users.noreply.github.com>
2023-06-17 23:51:54 +0200

    add if in function printText and added comments

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ let delay = 500 / speedOriginal.value;
 function printText(){
     heading.innerText = text.slice(0, idLetter);
 
+    // zvýší idletter vždy o 1
     idLetter++;
+
+    // vyresetuje idletter zpět na 1 a text se začne vypisovat znovu
+    if(idLetter > text.length){
+        idLetter = 1;
+    }
 
     // spustí funkci printText se zpožděním delay
     setTimeout(printText, delay);
