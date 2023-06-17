ba1c8b1aec8be4d1289e582c9fe8843e2bee0e80
Tomáš Stýblo <119765404+Tomiczech77@users.noreply.github.com>
2023-06-17 23:44:11 +0200

    new function printText

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const heading = document.getElementById("text");
 const speedOriginal = document.getElementById("speed");
 const text = "Welcome to my website :-)";
 let idLetter = 1;
-let speed = 500 / speedOriginal.value;
+let delay = 500 / speedOriginal.value;
 
+function printText(){
+    heading.innerText = text.slice(0, idLetter);
 
+    idLetter++;
+
+    // spustí funkci printText se zpožděním delay
+    setTimeout(printText, delay);
+}
+
+printText();
