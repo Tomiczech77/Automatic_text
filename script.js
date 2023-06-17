70fcfe1f0d0d1da137f59da58bffc30e9b9c30e4
Tomáš Stýblo <119765404+Tomiczech77@users.noreply.github.com>
2023-06-17 23:31:56 +0200

    add const, let

---
+++ script.js
@@ -0,0 +1,7 @@
+const heading = document.getElementById("text");
+const speedOriginal = document.getElementById("speed");
+const text = "Welcome to my website :-)";
+let idLetter = 1;
+let speed = 500 / speedOriginal.value;
+
+
